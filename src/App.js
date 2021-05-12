c9046c033a13122ac963bccd06708f7b39fc791b
Christine Yoo <christinecho.s@gmail.com>
2021-05-11 22:17:01 -0700

    added Switch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from './Pages/Dashboard/Dashboard';
 import Group from './Pages/Group/Group';
 import Landing from './Pages/Landing/Landing';
 import NotFound from './Pages/NotFound/NotFound';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SignUp from './Pages/SignUp/SignUp';
 
 class App extends Component {
@@ -19,13 +19,15 @@ class App extends Component {
     return (
       <main className='App'>
         <header>
-          <Route exact path='/signup' component={SignUp} />
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path='/all-posts' component={AllPosts} />
-          <Route exact path='/add-post' component={AddPost} />
-          <Route path='/group/:groupId' component={Group} />
-          <Route exact path='/' component={Landing} />
-          <Route component={NotFound} />
+          <Switch>
+            <Route exact path='/signup' component={SignUp} />
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/all-posts' component={AllPosts} />
+            <Route exact path='/add-post' component={AddPost} />
+            <Route path='/group/:groupId' component={Group} />
+            <Route exact path='/' component={Landing} />
+            <Route component={NotFound} />
+          </Switch>
         </header>
       </main>
     );
