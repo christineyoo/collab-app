c4fe76122ebf4746c9a0e42874a258ea4757d01c
Christine Yoo <christinecho.s@gmail.com>
2021-05-11 23:06:54 -0700

    removed switch and redirect

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import AddPost from './Pages/AddPost/AddPost';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Group from './Pages/Group/Group';
 import Landing from './Pages/Landing/Landing';
-import NotFound from './Pages/NotFound/NotFound';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import SignUp from './Pages/SignUp/SignUp';
 
 class App extends Component {
@@ -19,16 +18,12 @@ class App extends Component {
     return (
       <main className='App'>
         <header>
-          {/* <Switch> */}
           <Route exact path='/signup' component={SignUp} />
           <Route exact path='/dashboard' component={Dashboard} />
           <Route exact path='/all-posts' component={AllPosts} />
           <Route exact path='/add-post' component={AddPost} />
           <Route path='/group/:groupId' component={Group} />
           <Route exact path='/' component={Landing} />
-          {/* <Route path='/404' component={NotFound} />
-            <Redirect to='/404' /> */}
-          {/* </Switch> */}
         </header>
       </main>
     );
